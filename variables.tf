7f04567d4d67d3644786471195b1aaaa7be75a78
ziyashaw <52593780+ziyashaw@users.noreply.github.com>
2019-07-18 12:31:17 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "aws_access_key" {
+  default = "your-aws-access-key-here"
+}
+
+variable "aws_secret_key" {
+  default = "aws-secret-key-here"
+}
+
+variable "aws_region" {
+  default = "us-west-2"
+}
